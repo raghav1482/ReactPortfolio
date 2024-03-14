c946924bad14be00df213954ebac06921e4ad6b7
Raghvendra Tiwari <98442936+raghav1482@users.noreply.github.com>
2024-03-14 20:02:08 +0530

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,7 +8,7 @@ export default function Projects(){
     const myproj=[
         [proj1,'ChatIt:MERN Chat APP','https://github.com/raghav1482/Chatit','https://chait-online.netlify.app/','Chat It is a feature-rich chat application that provides a dynamic and interactive platform for real-time communication. Leveraging a robust technology stack, including Express, MongoDB, React, and Socket.io',['#MongoDB',' #react',' #Express',' #bootstrap',' #Socket.io' , '#JWT']] , 
         [proj2,'MovieAdda','https://github.com/raghav1482/MvADDA','https://movieadda-tan.vercel.app/',"Movie Adda, a MERN stack project, offers a seamless platform for users to explore, organize, and curate their favorite movies. Login, access detailed movie information from an external API, and effortlessly manage your film collection.","#reactjs #nodejs #expressjs #mongoDB #bcryptjs"]  ,
-         [proj3,'Text Utils','https://github.com/raghav1482/TEXTUTILS_','https://textutilsraghav.netlify.app/',"I've developed a user-friendly tool allowing you to effortlessly edit text. Paste your content and explore features like case conversion, binary transformation, and more.","#react #bootstrap"],];
+         [proj3,'BLogit','https://github.com/raghav1482/blogit','https://rgblogit.vercel.app/',"Blogit is built to simplify the process of creating and sharing blogs. It offers a user-friendly interface for writing, formatting, and publishing articles. With integrated features for user authentication and content management, Prompt ensures a smooth blogging experience for both writers and readers.","#react #Next.js #MongoDB #bootstrap"],];
 
 return(<>
     <div id="projects" data-aos="fade-up">
