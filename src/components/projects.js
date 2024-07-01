c2913dd056fe85ad3594f9f1cd50c08045d62edf
Raghvendra Tiwari <98442936+raghav1482@users.noreply.github.com>
2024-07-02 01:41:27 +0530

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,7 +6,7 @@ import proj3 from "../images/proj3.png";
 export default function Projects(){
 
     const myproj=[
-        [proj1,'ChatIt:MERN Chat APP','https://github.com/raghav1482/Chatit','https://chait-online.netlify.app/','Chat It is a feature-rich chat application that provides a dynamic and interactive platform for real-time communication. Leveraging a robust technology stack, including Express, MongoDB, React, and Socket.io',['#MongoDB',' #react',' #Express',' #bootstrap',' #Socket.io' , '#JWT']] , 
+        [proj1,'ChatIt:MERN Chat APP','https://github.com/raghav1482/Chatit','https://chatit-gs2u.onrender.com','Chat It is a feature-rich chat application that provides a dynamic and interactive platform for real-time communication. Leveraging a robust technology stack, including Express, MongoDB, React, and Socket.io',['#MongoDB',' #react',' #Express',' #bootstrap',' #Socket.io' , '#JWT']] , 
         [proj2,'MovieAdda','https://github.com/raghav1482/MvADDA','https://movieadda-tan.vercel.app/',"Movie Adda, a MERN stack project, offers a seamless platform for users to explore, organize, and curate their favorite movies. Login, access detailed movie information from an external API, and effortlessly manage your film collection.","#reactjs #nodejs #expressjs #mongoDB #bcryptjs"]  ,
          [proj3,'BLogit','https://github.com/raghav1482/blogit','https://rgblogit.vercel.app/',"Blogit is built to simplify the process of creating and sharing blogs. It offers a user-friendly interface for writing, formatting, and publishing articles. With integrated features for user authentication and content management, Prompt ensures a smooth blogging experience for both writers and readers.","#react #Next.js #MongoDB #bootstrap"],];
 
